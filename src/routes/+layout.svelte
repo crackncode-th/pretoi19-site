2c86f848ba67e7f0eb9c9707f75d555bb9a85072
MasterIceZ <64594596+MasterIceZ@users.noreply.github.com>
2023-04-04 23:27:33 +0700

    feat: inital:tada:

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import "../app.css";
+</script>
+
+<main class="min-h-[calc(100vh-18rem)] p-4">
+	<slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<main>
+	<h1 class="text-5xl font-bold text-red-500">Pre TOI</h1>
+</main>
