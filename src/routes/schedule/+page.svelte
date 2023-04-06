e6f624b8822e99ad9bf406c3b0d94d82e7183c75
MasterIceZ <64594596+MasterIceZ@users.noreply.github.com>
2023-04-06 21:44:38 +0700

    feat: add schedule page

--- src/routes/schedule/+page.svelte
+++ src/routes/schedule/+page.svelte
@@ -1,10 +1,71 @@
 <main class="page">
 	<h1 class="page-title">กำหนดการ</h1>
-	<h2 class="font-bold">Coming Soon...</h2>
+	<h2 class="font-bold">วันที่และเวลาทั้งหมดจะอยู่ในเวลามาตรฐานประเทศไทย (GMT+7)</h2>
+
+	<div class="flex justify-center">
+		<table class="m-8 mx-auto border-collapse border border-white">
+			<thead>
+				<tr>
+					<th class="grid-head">วันที่</th>
+					<th class="grid-head">เวลา</th>
+					<th class="grid-head">กิจกรรม</th>
+				</tr>
+			</thead>
+			<tr>
+				<td>xx เมษายน 2566</td>
+				<td>xx:xx</td>
+				<td>เปิดรับสมัคร</td>
+			</tr>
+			<tr>
+				<td>xx พฤษภาคม 2566</td>
+				<td>xx:xx</td>
+				<td>ปิดรับสมัคร</td>
+			</tr>
+			<tr>
+				<td>xx พฤษภาคม 2566</td>
+				<td>xx:xx</td>
+				<td>ผู้เข้าแข่งขันทดสอบระบบ</td>
+			</tr>
+			<tr class="font-bold text-green-300">
+				<td>xx พฤษภาคม 2566</td>
+				<td>xx:xx - xx:xx</td>
+				<td>การแข่งขันวันที่ 1</td>
+			</tr>
+			<tr>
+				<td>xx พฤษภาคม 2566</td>
+				<td>xx:xx - xx:xx</td>
+				<td>Analysis Mode(Day 1)</td>
+			</tr>
+			<tr class="font-bold text-green-300">
+				<td>xx พฤษภาคม 2566</td>
+				<td>xx:xx - xx:xx</td>
+				<td>การแข่งขันวันที่ 2</td>
+			</tr>
+			<tr>
+				<td>xx พฤษภาคม 2566</td>
+				<td>xx:xx - xx:xx</td>
+				<td>Analysis Mode(Day 2)</td>
+			</tr>
+			<tr>
+				<td>xx พฤษภาคม 2566</td>
+				<td>xx:xx</td>
+				<td>ประกาศผลการแข่งขัน</td>
+			</tr>
+		</table>
+	</div>
+
+	<h2 class="text-red-500">*เวลาดังกล่าวเป็นการคาดการณ์โดยประมาณ อาจมีการเปลี่ยนแปลงได้</h2>
 </main>
 
 <style lang="postcss">
 	h2 {
 		@apply mt-4 text-2xl;
+	}
+	.grid-head {
+		@apply text-xl font-bold;
+	}
+	th,
+	td {
+		@apply border p-2 text-xl;
 	}
 </style>
