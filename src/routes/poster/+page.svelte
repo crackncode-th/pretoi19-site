db67e9bf34454b22b4cbcad7a023d37c13f0e206
MasterIceZ <64594596+MasterIceZ@users.noreply.github.com>
2023-04-14 20:38:51 +0700

    feat: create poster page :tada:

---
+++ src/routes/poster/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import ext from "$lib/ext";
+	import RedButton from "$components/elements/RedButton.svelte";
+</script>
+
+<main class="page">
+	<div class="grid justify-center">
+		<img src="/poster.png" alt="poster" />
+		<RedButton href="/">Back to Home</RedButton>
+	</div>
+</main>
